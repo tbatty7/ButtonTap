6eec8995761e38108d5b845940c5ebbfa14cc0eb
Tim Batty <tbatty7@gmail.com>
2022-02-04 15:30:43 -0500

    Implemented test that simulates someone hitting a button

--- ButtonTap/ViewController.swift
+++ ButtonTap/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet private var button: UIButton!
+    @IBOutlet private(set) var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- ButtonTapTests/ViewControllerTests.swift
+++ ButtonTapTests/ViewControllerTests.swift
@@ -3,7 +3,11 @@ import XCTest
 
 final class ViewControllerTests: XCTestCase {
 
-    func test_zero() throws {
+    func test_tappingButton() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        viewController.loadViewIfNeeded()
         
+        viewController.button.sendActions(for: .touchUpInside)
     }
 }
