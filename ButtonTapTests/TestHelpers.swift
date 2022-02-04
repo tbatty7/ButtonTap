6cf4d34de9ad9795926d5368543ebc71328d77d2
Tim Batty <tbatty7@gmail.com>
2022-02-04 15:38:18 -0500

    Added test helper method to tap a UIBarButtonItem as well

--- ButtonTapTests/TestHelpers.swift
+++ ButtonTapTests/TestHelpers.swift
@@ -10,3 +10,7 @@ import UIKit
 func tap(_ button: UIButton) {
     button.sendActions(for: .touchUpInside)
 }
+
+func tap(_ button: UIBarButtonItem ) {
+    _ = button.target?.perform(button.action, with: nil)
+}
