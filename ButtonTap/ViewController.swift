3f573fac4a53a19261e351a89e9e50aad6ab93c0
Tim Batty <tbatty7@gmail.com>
2022-02-04 15:21:19 -0500

    Wired up a button along with an onclick event and removed the slow, pointless UITests

--- ButtonTap/ViewController.swift
+++ ButtonTap/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction private func buttonTap() {
+        print(">>>>>> Button was tapped")
+    }
 
 }
 
---
+++ ButtonTapTests/ViewControllerTests.swift
@@ -0,0 +1,9 @@
+@testable import ButtonTap
+import XCTest
+
+final class ViewControllerTests: XCTestCase {
+
+    func test_zero() throws {
+        
+    }
+}
